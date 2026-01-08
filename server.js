02e94bb2980e857ca6191c35fed22642dce0b5ce
Randy <24047653@gmail.com>
2026-01-08 14:33:32 +0800

    ww

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const dbConfig = {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.BD_PORT,
+    port: process.env.DB_PORT,
     waitForConnections: true,
     connectionLimit: 100,
     queueLimit:0,
