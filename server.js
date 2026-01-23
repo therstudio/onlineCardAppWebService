b71fa912e38d8b6cda4f05b2a7c203f0667d71de
irfan <irfanimr.dev@gmail.com>
2026-01-23 21:03:11 +0800

    add frontend web to allowedorigins

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-const express = require('express');
-const mysql = require('mysql2/promise');
-require('dotenv').config();
+const express = require("express");
+const mysql = require("mysql2/promise");
+require("dotenv").config();
 const port = 3000;
 
 //database config info
 const dbConfig = {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
-    waitForConnections: true,
-    connectionLimit: 100,
-    queueLimit:0,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 100,
+  queueLimit: 0,
 };
 
 const app = express();
@@ -20,7 +20,7 @@ const app = express();
 app.use(express.json());
 
 app.listen(port, () => {
-    console.log('Server running on port', port);
+  console.log("Server running on port", port);
 });
 
 const cors = require("cors");
@@ -28,7 +28,7 @@ const cors = require("cors");
 const allowedOrigins = [
   "http://localhost:3000",
   "https://card-app-smoky.vercel.app",
-  // "https://YOUR-frontend.onrender.com"
+  "https://card-app-starter-sable.vercel.app/cards",
 ];
 
 app.use(
@@ -45,28 +45,35 @@ app.use(
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: false,
-  })
+  }),
 );
 
-app.get('/allcards', async (req, res) => {
-    try {
-        let connection = await mysql.createConnection(dbConfig);
-        const [rows] = await connection.execute('SELECT * FROM defaultdb.cards');
-        res.json(rows);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ message: err });
-    }
+app.get("/allcards", async (req, res) => {
+  try {
+    let connection = await mysql.createConnection(dbConfig);
+    const [rows] = await connection.execute("SELECT * FROM defaultdb.cards");
+    res.json(rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: err });
+  }
 });
 
-app.post('/addcard', async (req, res) => {
-    const { card_name, card_pic} = req.body;
-    try {
-        let connection = await mysql.createConnection(dbConfig);
-        await connection.execute('INSERT INTO cards (card_name, card_pic) VALUES (?, ?)', [card_name, card_pic]);
-        res.status(201).json({message: 'Card '+card_name+'added sucessfully'});
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ message: 'Server error - could not add card '+card_name});
-    }
+app.post("/addcard", async (req, res) => {
+  const { card_name, card_pic } = req.body;
+  try {
+    let connection = await mysql.createConnection(dbConfig);
+    await connection.execute(
+      "INSERT INTO cards (card_name, card_pic) VALUES (?, ?)",
+      [card_name, card_pic],
+    );
+    res
+      .status(201)
+      .json({ message: "Card " + card_name + "added sucessfully" });
+  } catch (err) {
+    console.error(err);
+    res
+      .status(500)
+      .json({ message: "Server error - could not add card " + card_name });
+  }
 });
